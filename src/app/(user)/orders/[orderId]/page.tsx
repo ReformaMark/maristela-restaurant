a2c965e0a4d500411f626d883769a47daae952ae
Mark <99254035+ReformaMark@users.noreply.github.com>
2024-10-26 01:56:17 +0800

    revamp ui for orders and order status

--- src/app/(user)/orders/[orderId]/page.tsx
+++ src/app/(user)/orders/[orderId]/page.tsx
@@ -21,7 +21,16 @@ import { Button } from '@/components/ui/button'
 import { useState } from 'react'
 import { toast } from 'sonner'
 import RatingStars from '@/components/Stars'
+import { CheckCircle, Clock, Truck, Check, XCircle } from 'lucide-react';
 
+export type StatusType = 'Pending' | 'Confirmed' | 'Out for Delivery' | 'Completed' | 'Cancelled';
+export const statusIcons: Record<StatusType, JSX.Element> = {
+    Pending: <Clock className="w-5 h-5 md:size-16" />,
+    Confirmed: <CheckCircle className="w-5 h-5 md:size-16" />,
+    'Out for Delivery': <Truck className="w-5 h-5 md:size-16" />,
+    Completed: <Check className="w-5 h-5 md:size-16" />,
+    Cancelled: <XCircle className="w-5 h-5 md:size-16" />
+};
 function TransactionPage({
     params
 }:{
@@ -29,6 +38,7 @@ function TransactionPage({
         orderId: Id<'transactions'>
     }
 }) {
+    
     const transaction = useQuery(api.transactions.getTransaction, {transactionid: params.orderId})
     const cancel = useMutation(api.transactions.cancelTransaction)
     const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -142,92 +152,86 @@ function TransactionPage({
             </Dialog>
         </div>
         <div className="bg-white p-3 md:p-10 rounded-lg shadow-md">
-            <div className="flex items-center gap-x-2 justify-center text-text font-semibold">
-                <div className={`${Status === "Pending" ? "text-primary" : "text-text"} text-[0.5rem] md:text-sm`}>
-                    Pending
-                </div>
-                <div className="bg-text w-5 md:w-10 h-1 rounded-md"></div>
-                <div className={`${Status === "Confirmed" ? "text-primary" : "text-text"} text-[0.5rem] md:text-sm`}>
-                    Confirmed
-                </div>
-                <div className="bg-text w-5 md:w-10 h-1 rounded-md"></div>
-                <div className={`${Status === "Out for Delivery" ? "text-primary" : "text-text"} text-[0.5rem] md:text-sm`}>
-                    Out for delivery
-                </div>
-                <div className="bg-text w-5 md:w-10 h-1 rounded-md"></div>
-                <div className={`${Status === "Completed" ? "text-primary" : "text-text"} text-[0.5rem] md:text-sm`}>
-                    Completed
-                </div>
-                <div className="bg-text w-5 md:w-10 h-1 rounded-md"></div>
-                <div className={`${Status === "Cancelled" ? "text-primary" : "text-text"} text-[0.5rem] md:text-sm`}>
-                    Cancelled
-                </div>
-            </div>
-            <div className="">
-                <div className="w-full space-y-2">
-                    <div className="grid grid-cols-12 w-full py-2 md:text-lg text-xs border-b border-b-black">
-                        <h1 className='col-span-6'>Product</h1>
-                        <h1 className='col-span-2'>Price</h1>
-                        <h1 className='col-span-2'>Quantity</h1>
-                        <h1 className='col-span-2 text-right'>Total</h1>
-                    </div>
-                    <div className="min-h-[50vh]">
-                    {transaction?.orders.map((order)=>(
-                        <div key={order._id} className="py-3 text-xs md:text-sm border-b border-b-gray-100">
-                            <div className="grid grid-cols-12 w-full items-center ">
-                                    <div className='col-span-6 flex gap-x-4 items-center'>
-                                    <Image src={order ? order.url !== null ? order.url : "" : ""} alt='Image' width={200} height={200} className='size-10 object-contain'/>
-                                    <h1>{order.menuName}</h1>
-                                    </div>
-                                <h1 className='col-span-2'>{formatPrice(order.menu? order.menu.price : 0)}</h1>
-                                <h1 className='col-span-2'>{order.quantity}</h1>
-                                <h1 className='col-span-2 text-right'>{formatPrice(order.totalPrice)}</h1>
-                            </div>
-                            {transaction.status === 'Completed' && (
-                                <div className="flex items-center gap-x-1">
-                                    <h1 className='text-text text-sm'>Rate this product: </h1>
-                                    <RatingStars edit={true} size={30} menuId={order.menuId} transactionId={transaction._id}/>
-
-                                </div>
-                            )}
+            <div className="flex items-center justify-evenly gap-3 md:gap-6"> {/* Adjusted gap for better spacing */}
+                {(['Pending', 'Confirmed', 'Out for Delivery', 'Completed', 'Cancelled'] as StatusType[]).map((status) => (
+                    <div key={status} className="flex flex-col justify-center items-center">
+                        <div className={`flex justify-center items-center bg-${Status === status ? "primary" : "gray-300"} ${Status === status ? "text-white" : "text-black"} size-10 md:size-20 rounded-full transition duration-300`}>
+                            {statusIcons[status]}
                         </div>
-                    ))}
+                        <div className={`${Status === status ? "text-primary" : "text-gray-700"} text-xs md:text-sm mt-1`}>
+                            {status}
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="p-4 bg-white shadow-md rounded-lg">
+                <div className="w-full space-y-2">
+                    {/* Header */}
+                    <div className="grid grid-cols-12 w-full py-2 text-lg text-gray-700 border-b border-b-gray-300">
+                        <h1 className='col-span-6 font-semibold'>Product</h1>
+                        <h1 className='col-span-2 font-semibold'>Price</h1>
+                        <h1 className='col-span-2 font-semibold'>Quantity</h1>
+                        <h1 className='col-span-2 font-semibold text-right'>Total</h1>
+                    </div>
+                    {/* Order List */}
+                    <div className="min-h-[50vh]">
+                        {transaction?.orders.map((order) => (
+                            <div key={order._id} className="py-3 text-sm border-b border-b-gray-100">
+                                <div className="grid grid-cols-12 w-full items-center">
+                                    <div className='col-span-6 flex gap-x-4 items-center'>
+                                        <Image src={order.url || ''} alt='Image' width={200} height={200} className='size-10 object-contain rounded-md shadow-sm' />
+                                        <h1 className="text-gray-800">{order.menuName}</h1>
+                                    </div>
+                                    <h1 className='col-span-2'>{formatPrice(order.menu?.price || 0)}</h1>
+                                    <h1 className='col-span-2'>{order.quantity}</h1>
+                                    <h1 className='col-span-2 text-right'>{formatPrice(order.totalPrice)}</h1>
+                                </div>
+                                {transaction.status === 'Completed' && (
+                                    <div className="flex items-center gap-x-1 mt-2">
+                                        <h1 className='text-gray-600 text-sm'>Rate this product:</h1>
+                                        <RatingStars edit={true} size={30} menuId={order.menuId} transactionId={transaction._id} />
+                                    </div>
+                                )}
+                            </div>
+                        ))}
                     </div>
                 </div>
-                <div className='flex flex-col font-semibold'>
+
+                {/* Summary Section */}
+                <div className='flex flex-col font-semibold mt-4'>
                     <div className='flex w-full justify-between'>
-                        <h1>Shipping Fee</h1>
+                        <h1 className='text-gray-700'>Shipping Fee</h1>
                         <h1>{formatPrice(80)}</h1>
                     </div>
                     <div className='flex w-full justify-between'>
-                        <h1 className='text-lg'>Total</h1>
-                        <h1 className='text-lg'>{formatPrice(computeCost(transaction?.orders) + 80)}</h1>
+                        <h1 className='text-lg text-gray-800'>Total</h1>
+                        <h1 className='text-lg text-gray-800'>{formatPrice(computeCost(transaction?.orders) + 80)}</h1>
                     </div>
-                    {transaction && transaction.status === "Pending" ? (
+                    {transaction && transaction.status === "Pending" && (
                         <div className="flex flex-col md:items-end md:justify-end mt-5">
-                           <Dialog open={isOpen}>
-                               <DialogTrigger> <Button variant={'destructive'} onClick={()=> setIsOpen(true)} className=''>Cancel Order</Button></DialogTrigger>
-                               <DialogContent>
-                                   <DialogHeader>
-                                   <DialogTitle>Cancel Order?</DialogTitle>
-                                   <DialogDescription>
-                                       Once canceled, your order will not be processed or delivered. Please note that if your order is already confirmed, cancellation might not be possible.
-                                   </DialogDescription>
-                                   </DialogHeader>
-                                   <DialogFooter>
-                                       <Button variant={'ghost'} onClick={()=> setIsOpen(false)} className='w-full md:w-1/4'>No</Button>
-                                       <Button variant={'destructive'} onClick={handleCancel} className='w-full md:w-1/4'>Yes</Button>
-                                   </DialogFooter>
-                               </DialogContent>
-                           </Dialog>
-                           <h1 className='text-text text-xs text-right'>* You will not be able to cancel once the order is being confirmed.</h1>
-                       </div>
-                    ):(
-                        <></>
+                            <Dialog open={isOpen}>
+                                <DialogTrigger>
+                                    <Button variant={'destructive'} onClick={() => setIsOpen(true)}>Cancel Order</Button>
+                                </DialogTrigger>
+                                <DialogContent>
+                                    <DialogHeader>
+                                        <DialogTitle>Cancel Order?</DialogTitle>
+                                        <DialogDescription>
+                                            Once canceled, your order will not be processed or delivered. Please note that if your order is already confirmed, cancellation might not be possible.
+                                        </DialogDescription>
+                                    </DialogHeader>
+                                    <DialogFooter>
+                                        <Button variant={'ghost'} onClick={() => setIsOpen(false)} className='w-full md:w-1/4'>No</Button>
+                                        <Button variant={'destructive'} onClick={handleCancel} className='w-full md:w-1/4'>Yes</Button>
+                                    </DialogFooter>
+                                </DialogContent>
+                            </Dialog>
+                            <h1 className='text-gray-600 text-xs text-right'>* You will not be able to cancel once the order is being confirmed.</h1>
+                        </div>
                     )}
-                 
                 </div>
             </div>
+
             
         </div>
     </div>
