bedfa9225c0437266cf57b2c9c074f7fcb703627
MarkR <99254035+ReformaMark@users.noreply.github.com>
2024-12-05 03:53:47 +0800

    fix: test 5.2

--- convex/menus.ts
+++ convex/menus.ts
@@ -317,21 +317,21 @@ export const personalizedRecommendation = query({
         console.log("categories: ",categories)
 
         const topMenusByCategory = await asyncMap(categories, async (category) => {
-            const menus = await ctx.db.query('menus').filter(q => q.eq(q.field('category'), category)).collect();
-            if (!menus || menus.length === 0) {
-                throw new Error(`No menus found for category ${category}`);
-            }
-            // console.log(menus)
-            const orderCounts = await asyncMap(menus, async (menu) => {
-                const menuId = menu._id;
-                const orders = await ctx.db.query('orders').filter(q => q.eq(q.field('menuId'), menuId)).collect();
-                return {
-                    menuId,
-                    numberOfOrders: orders.length,
-                };
-            });
+            // const menus = await ctx.db.query('menus').filter(q => q.eq(q.field('category'), category)).collect();
+            // if (!menus || menus.length === 0) {
+            //     throw new Error(`No menus found for category ${category}`);
+            // }
+            // // console.log(menus)
+            // const orderCounts = await asyncMap(menus, async (menu) => {
+            //     const menuId = menu._id;
+            //     const orders = await ctx.db.query('orders').filter(q => q.eq(q.field('menuId'), menuId)).collect();
+            //     return {
+            //         menuId,
+            //         numberOfOrders: orders.length,
+            //     };
+            // });
 
-            return {menus, orderCounts}
+            // return {menus, orderCounts}
 
             // const sortedOrderCounts = orderCounts.sort((a, b) => b.numberOfOrders - a.numberOfOrders);
             // const topThreeMenus = sortedOrderCounts.slice(0, 3);
